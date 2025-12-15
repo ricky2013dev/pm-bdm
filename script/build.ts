db439d0892ec521dc7f769937f58969054d0c9bf
Ricky Kim <rickykim2013.dev@gmail.com>
2025-12-14 18:02:55 -0600

    t

--- script/build.ts
+++ script/build.ts
@@ -54,8 +54,8 @@ await esbuild({
     define: {
       "process.env.NODE_ENV": '"production"',
     },
-    banner: {
-      js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);const __filename = fileURLToPath(import.meta.url);const __dirname = dirname(__filename);",
+banner: {
+      js: "import { createRequire } from 'module';import { fileURLToPath } from 'url';import { dirname } from 'path';const require = createRequire(import.meta.url);const __filename = fileURLToPath(import.meta.url);const __dirname = dirname(__filename);",
     },
     minify: true,
     external: externals,
